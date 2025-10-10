409e568577dba6af096862d1d397a18fa6d2714c
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-10-10 00:08:49 -0700

    route between signin and signup

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,7 +1,6 @@
 import { images } from '@/constants'
 import React from 'react'
-import { Image, Text, TouchableOpacity } from 'react-native'
-import { View } from 'react-native-reanimated/lib/typescript/Animated'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 function CartButton() {
     const totalItems = 10
