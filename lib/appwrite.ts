d95204752a5547819aed286f0cb5f16ac702131b
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-10-14 19:36:37 -0700

    implement half top of login page

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.jsm.orderD",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68eddc4a00361c42ce82',
+    userCollectionId: "68ef041c0019d5621fe1"
+}
