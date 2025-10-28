21e9d84c9d6a5b94564774c072952f953e8d2592
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-10-27 22:23:06 -0700

    render sign-in and sign-up

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,3 @@
-import CustomButton from '@/components/CustomButton'
-import CustomInput from '@/components/CustomInput'
 import { images } from '@/constants'
 import { Slot } from 'expo-router'
 import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
@@ -12,16 +10,9 @@ export default function _layout() {
                 <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25 }}>
                     <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode="stretch" />
                     <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10" />
+                    <Slot />
                 </View>
-                <CustomInput
-                    placeholder="Enter your email"
-                    value={""}
-                    onChangeText={(text: string) => { }}
-                    label="Email"
-                    keyboardType="email-address"
-                />
-                <CustomButton />
-                <Slot />
+
             </ScrollView>
         </KeyboardAvoidingView>
     )
