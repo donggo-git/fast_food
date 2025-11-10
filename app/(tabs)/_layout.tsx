440021602e0ec14790bed4792461888e74b483dc
Dong Nguyen <dlttthvlhhth@gmail.com>
2025-11-09 23:16:07 -0800

    implement and style tab in the home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,58 @@
-import { Redirect, Slot } from 'expo-router';
+import { Redirect, Tabs } from 'expo-router';
 import React from 'react';
 
+import { images } from '@/constants';
 import useAuthStore from '@/store/auth.store';
+import { TabBarIconProps } from '@/type';
 import 'nativewind';
+import { Image, Text, View } from 'react-native';
+
+
+const TabBarIcon = ({ focused, icon, title, }: TabBarIconProps) => (
+    <View className="tab-icon">
+        <Image source={icon} className='size-7' resizeMode='contain' tintColor={focused ? '#FE8C00' : '#5D5F6D'} />
+        <Text className={`text-sm font-bold ${focused ? 'text-primary' : 'text-gray-200'}`}>{title}</Text>
+    </View>
+)
 
 export default function TabLayout() {
     const { isAuthenticated } = useAuthStore();
     if (!isAuthenticated)
         return <Redirect href="/sign-in" />
 
-    return <Slot />
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    borderTopLeftRadius: 50,
+                    borderTopRightRadius: 50,
+                    borderBottomLeftRadius: 50,
+                    borderBottomRightRadius: 50,
+                    marginHorizontal: 20,
+                    height: 80,
+                    position: 'absolute',
+                    bottom: 40,
+                    backgroundColor: "white",
+                    shadowColor: '#1a1a1a',
+                    shadowOffset: {
+                        width: 0,
+                        height: 0
+                    },
+                    shadowOpacity: 0.1,
+                    shadowRadius: 4,
+                    elevation: 5
+                }
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} title="Home" icon={images.home} />
+                }} />
+        </Tabs >
+    )
 
 }
